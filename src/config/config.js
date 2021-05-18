2a6c99036abcc0f041a4c27ad443244acbd2ba38
Luis Fernando Flores dos Santos <luis.santos@govbr.com.br>
2021-05-18 18:30:25 -0300

    Docker_Compose

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 module.exports = {
   dialect: 'mysql',
-  host: process.env.NODE_ENV === 'dockerDev' ? process.env.HDOCKER : process.env.HOST,
+  host: process.env.HOST,
   username: process.env.USER,
-  password: process.env.NODE_ENV === 'dockerDev' ? process.env.PASSWORD : process.env.PASSWORD_LOCAL,
+  password: process.env.PASSWORD,
   database: process.env.DATABASE,
+  port: process.env.DB_PORT,
   define: {
     timestamps: true
   },
